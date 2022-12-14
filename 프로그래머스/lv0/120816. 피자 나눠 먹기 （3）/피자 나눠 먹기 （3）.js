3b1f7d11197a803f3e0e5ab192a4ab87badeeee8
Chany <111185089+chanheek@users.noreply.github.com>
2022-12-14 23:49:33 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -0,0 +1,12 @@
+function solution(slice, n) {
+    return n%slice === 0 ? Math.ceil(n/slice) : Math.ceil(n/slice)
+}
+
+
+
+// n%s === 0 ? Math.ceil(n%s) : 
+// s = 10 n =1 r = 1 n<=10
+    
+    
+// s = 7 n = 10  0.7 = 2 
+// s = 9 n = 22 2.xxx = 3 dzdz
