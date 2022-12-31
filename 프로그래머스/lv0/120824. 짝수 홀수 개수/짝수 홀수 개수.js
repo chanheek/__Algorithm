d4903bb2008f6c57dbf7879a1f825ddfad6bbbb7
Chany <111185089+chanheek@users.noreply.github.com>
2022-12-31 17:46:58 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,17 +1,5 @@
 function solution(num_list) {
-    let result = [];
-    let J = 0;
-    let H = 0;
-    for (let i = 0; i<num_list.length; i ++) {
-        if(num_list[i]%2 ===0) {
-            J++
-        }else {
-            H++
-        }
-     }
-        result.push(J)
-    result.push(H)
-    return result
+    let even = num_list.filter(e => e%2===0).length 
+    
+    return [even,num_list.length-even]
 }
-// 짝 ++ 홀 ++
-// 
