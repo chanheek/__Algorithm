1398c071e710945479156afab4984f425da07bcd
Chany <111185089+chanheek@users.noreply.github.com>
2023-02-02 19:11:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    let result = '';
+    let words = s.split(' ');
+    
+    for (let i = 0; i < words.length; i++) {
+        for(let j = 0; j < words[i].length; j++) {
+            if(j % 2 === 0) {
+                result += words[i][j].toUpperCase();
+            } else {
+                result += words[i][j].toLowerCase();
+            }
+        }
+        if (i < words.length -1) {
+            result += ' ';
+        }
+    }
+    return result;
+}
