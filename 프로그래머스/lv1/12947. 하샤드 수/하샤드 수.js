1f6e982db5d2461fc7485d4e7c1fedfe079ce960
Chany <111185089+chanheek@users.noreply.github.com>
2023-04-06 21:08:10 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,12 @@
+function solution(x) {
+    
+    let num = x.toString().split('');
+
+    let sum = 0;
+
+    for(let i = 0; i<num.length; i++) {
+      sum += Number(num[i]);
+    }
+    
+    return x%sum === 0 ? true : false
+}
