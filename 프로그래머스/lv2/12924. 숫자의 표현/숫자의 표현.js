6d6281594a91727f828b27f65a17664165bb6f73
Chany <111185089+chanheek@users.noreply.github.com>
2023-04-15 22:39:32 +0900

    [level 2] Title: 숫자의 표현, Time: 1.55 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,20 @@
+function solution(n) {
+    
+  let result = 0;
+
+  for (let i = 1; i <= n; i++) {
+    let sum = 0;
+    for (let j = i; j <= n; j++) {
+      sum += j;
+
+      if (sum === n) {
+        result++;
+        break;
+      } else if (sum > n) {
+        break;
+      }
+    }
+  }
+
+  return result;
+}
