a44be281a5467e9d06485633f46d7f9f3390ffbf
Chany <111185089+chanheek@users.noreply.github.com>
2023-07-14 18:03:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,10 +1,16 @@
 function solution(absolutes, signs) {
     
-    let result =0;
+    let result = 0;
 
-    for(let i =0; i<absolutes.length;i++) {
-        if(signs[i] === true) result += absolutes[i]
-        else result += -absolutes[i]
-    }
+// 사인 배열을 for 문 돌려서 false면 해당 인덱스 값에 -1 할당
+
+
+for(let i =0; i< signs.length; i++) {
+  if(!signs[i]) {
+    absolutes[i] = -absolutes[i]
+  }
+  result += absolutes[i]
+}
+
     return result
 }
