d856e27f9ca7b017e03f445dbb72cb49eaa763f1
Chany <111185089+chanheek@users.noreply.github.com>
2023-08-01 18:14:49 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,11 @@
+let fs = require('fs')
+
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+let arr= input[0].split(' ')
+
+let a = Number(arr[0])
+let b= Number(arr[1])
+
+console.log(a*b)
+
