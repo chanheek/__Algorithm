c8375c29e3a65881664f6ffe9998d42f03e7f95e
Chany <111185089+chanheek@users.noreply.github.com>
2023-12-01 19:24:44 +0900

    [level 2] Title: 올바른 괄호, Time: 5.80 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(s[i]);
+        } else if (s[i] === ')') {
+            if (stack.length === 0) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
