ece41377dec8a6e0a6d1cd084508a26d245ff6ab
Chany <111185089+chanheek@users.noreply.github.com>
2023-12-08 16:25:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.56 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,12 +2,11 @@ function solution(arr){
     
     let result = [];
 
-    for(let i = 0; i<arr.length; i++) {
-      if(arr[i]===arr[i+1]) {
-        continue
-      } else {
-        result.push(arr[i])
-      }
-    }
+   
+for(let i = 0; i<arr.length; i++) {
+  if(arr[i] !== arr[i+1]) {
+    result.push(arr[i])
+  }
+}
     return result
 }
