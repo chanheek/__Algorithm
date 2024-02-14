5b440a152d0080929e7a9ee6c2bb2a21d9695d93
Chany <111185089+chanheek@users.noreply.github.com>
2024-02-14 19:46:38 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,15 +1,11 @@
 function solution(array, commands) {
+   let result = [];
     
-    
-let result = [];
-
-
-
-for(let i =0; i<commands.length; i++) {
-  const [s,e,k] = commands[i]
-  const arr = array.slice(s-1,e).sort((a,b) => a-b)
-  result.push(arr[k-1])
-}
-    return result
-
+    for(let i = 0; i<commands.length; i++) {
+        const [s,e,n] = commands[i]
+        const test = array.slice(s-1,e).sort((a,b) => a-b)
+        const out = test[n-1]
+        result.push(out)
+    }
+        return result
 }
